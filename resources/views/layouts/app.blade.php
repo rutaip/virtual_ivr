7deee976c1cba8f0b85d8b5bd63a22994ac3b35d
Erick Nava <erick.nava@rutaip.net>
2017-05-16 11:19:34 -0500

    Welcome Site Created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,87 +1,86 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Fastcode</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Bootstrap 3.3.6 -->
+{!! Html::style('css/bootstrap.min.css') !!}
+{!! Html::style('css/AdminLTE.min.css') !!}
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<!-- Font Awesome -->
+{!! Html::style('css/font-awesome.min.css') !!}
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
+<!-- Custom styles for this template  -->
+{!! Html::style('css/navbar.css') !!}
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+{!! Html::style('css/ie10-viewport-bug-workaround.css') !!}
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+<div class="container">
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=" {{ url("/") }}">{{ config('app.name') }}</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                </ul>
+                @if (Route::has('login'))
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @if (Auth::check())
+                            <li><a href="{{ url('/home') }}">Inicio</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
+                            <li><a href="{{ url('/login') }}">Ingresar</a></li>
+                            <li><a href="{{ url('/register') }}">Registarse</a></li>
                         @endif
                     </ul>
-                </div>
-            </div>
-        </nav>
+                @endif
 
-        @yield('content')
-    </div>
+            </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+    </nav>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+@yield('content')
+
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- jQuery 2.2.3 -->
+{!! Html::script('js/jquery-2.2.3.min.js') !!}
+<!-- Bootstrap 3.3.6 -->
+{!! Html::script('js/bootstrap.min.js') !!}
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+{!! Html::script('js/ie10-viewport-bug-workaround.js') !!}
 </body>
 </html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,71 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+@extends('layouts.app')
 
-    <title>Fastcode</title>
-
-    <!-- Bootstrap 3.3.6 -->
-{!! Html::style('css/bootstrap.min.css') !!}
-{!! Html::style('css/AdminLTE.min.css') !!}
-
-<!-- Font Awesome -->
-{!! Html::style('css/font-awesome.min.css') !!}
-
-<!-- Custom styles for this template  -->
-{!! Html::style('css/navbar.css') !!}
-
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-{!! Html::style('css/ie10-viewport-bug-workaround.css') !!}
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-<div class="container">
-
-    <!-- Static navbar -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                        aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                </ul>
-                @if (Route::has('login'))
-                    <ul class="nav navbar-nav navbar-right">
-                        @if (Auth::check())
-                            <li><a href="{{ url('/home') }}">Inicio</a></li>
-                        @else
-                            <li><a href="{{ url('/login') }}">Ingresar</a></li>
-                            <li><a href="{{ url('/register') }}">Registarse</a></li>
-                        @endif
-                    </ul>
-                @endif
-
-            </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-    </nav>
-
+@section('content')
     <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="">
@@ -306,20 +241,4 @@
             </div>
         </div>
     </div>
-
-</div> <!-- /container -->
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<!-- jQuery 2.2.3 -->
-{!! Html::script('js/jquery-2.2.3.min.js') !!}
-<!-- Bootstrap 3.3.6 -->
-{!! Html::script('js/bootstrap.min.js') !!}
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-{!! Html::script('js/ie10-viewport-bug-workaround.js') !!}
-</body>
-</html>
-
+@endsection
