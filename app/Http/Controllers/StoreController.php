0a0bcd1149bd410a78990b4450899ea742b08039
Erick Nava <erick.nava@rutaip.net>
2017-05-26 19:32:34 -0500

    Creates Mercago Pago Payment Option

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -242,7 +242,11 @@ class StoreController extends Controller
 
         $mp->sandbox_mode(TRUE);
 
-        return 'ok';
+        $payment_info = $mp->get_payment_info($_GET["id"]);
+
+        if ($payment_info["status"] == 200) {
+            print_r($payment_info["response"]);
+        }
 
     }
 }
